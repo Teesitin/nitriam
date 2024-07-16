c54204d511ceae3d6f859b3063bb0f2f557ef958
T3rminaI <morrisjohn194@icloud.com>
2024-07-16 14:35:00 -0500

    Updated Database

--- src/routes/database/+page.svelte
+++ src/routes/database/+page.svelte
@@ -1,13 +1,38 @@
 <script lang="ts">
     import { db } from '$lib/firebase';
-    import { collection, getDocs } from 'firebase/firestore';
+    import { addDoc, collection, getDocs } from 'firebase/firestore';
 
+    // Declare 'products' as an array, this is reactive by default in Svelte
+    let products: any[] = [];
+    
+    let name: string='';
+    let description: string='';
+    let image: string='';
+    let price: string='';
+
+    // Function to load data
     async function loadData() {
         const querySnapshot = await getDocs(collection(db, "products"));
-        querySnapshot.forEach((doc) => {
-            console.log(doc.id, " => ", doc.data());
+        // Svelte will react to this reassignment
+        products = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        console.log(products);
+    }
+
+    async function createdata() {
+        await addDoc(collection(db, "products"), {
+            description: '',
+            image: '',
+            name: '',
+            price: 0
         });
     }
 </script>
 
-<button on:click={loadData}>Load Data</button>
+<button on:click={loadData} class="bg-slate-800 text-white p-2 hover:scale-110 focus:scale-110 m-10">Load Data</button>
+<button on:click={createdata} class="bg-slate-800 text-white p-2 hover:scale-110 focus:scale-110 m-10">Create Data</button>
+
+<div>
+    {#each products as product}
+        <div>{product.name}</div>
+    {/each}
+</div>
